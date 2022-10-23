7087e87de80d9a32f134ae02423ee52292f7acd0
ZACK! <mallowthepuff@gmail.com>
2022-10-23 13:02:34 -0500

    tasks 3 & 4

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,12 +1,13 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
-
 
 /**
  * Created by LaunchCode
@@ -17,6 +18,25 @@ public class SearchController {
 
     @GetMapping(value = "")
     public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchType.equals("all") || searchTerm.equals("") || searchTerm.equalsIgnoreCase("all")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        }
+        if (searchType.equals("all") && searchTerm.equalsIgnoreCase("")){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
         return "search";
     }
